4503f3d0e242b030976cc476c3e3131b0a8fee45
youli <www.xingjijiandui@aliyun.com>
2025-04-18 23:31:52 +0800

    可持久化存储的状态机
    可持久化存储的状态机

---
+++ ApprovalProcess.Core/ApprovalProcess.Core/StateMachine.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ApprovalProcess.Core
+{
+    public class StateMachine<TState, TTrigger>
+    {
+
+        public StateMachine(TState initialState)
+        {
+            InitialState = initialState;
+            State = initialState;
+        }
+
+        /// <summary>
+        /// 初始状态
+        /// </summary>
+        public TState InitialState { get; protected set; }
+
+        /// <summary>
+        /// 当前状态
+        /// </summary>
+        public TState State { get; private set; }
+
+        /// <summary>
+        /// 状态表达集
+        /// </summary>
+        public IDictionary<TState, StateRepresentation<TState, TTrigger>> StateConfiguration { get; set; } =
+            new Dictionary<TState, StateRepresentation<TState, TTrigger>>();
+
+        /// <summary>
+        /// 配置一个状态内容
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public StateRepresentation<TState, TTrigger> Configure(TState state)
+        {
+            if (!StateConfiguration.TryGetValue(state, out StateRepresentation<TState, TTrigger> result))
+            {
+                result = new StateRepresentation<TState, TTrigger>(state);
+                StateConfiguration.Add(state, result);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 触发
+        /// </summary>
+        public void Fire(TTrigger trigger)
+        {
+            var source = State;
+            var representativeState = GetRepresentation(source);
+
+            representativeState.TryFindBehaviour(trigger, out var triggerBehaviours);
+
+            var behaviour = triggerBehaviours.First();
+            State = behaviour.DestinationState;
+        }
+
+        private StateRepresentation<TState, TTrigger> GetRepresentation(TState state)
+        {
+            if (!StateConfiguration.TryGetValue(state, out StateRepresentation<TState, TTrigger> result))
+            {
+                result = new StateRepresentation<TState, TTrigger>(state);
+                StateConfiguration.Add(state, result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ApprovalProcess.Core/ApprovalProcess.Core/StateRepresentation.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApprovalProcess.Core
+{
+    /// <summary>
+    /// 状态表示
+    /// </summary>
+    public class StateRepresentation<TState, TTrigger>
+    {
+        public StateRepresentation(TState state)
+        {
+            State = state;
+        }
+
+        public TState State { get; set; }
+
+        public ICollection<TransitioningTriggerBehaviour<TState, TTrigger>> Behaviours { get; set; } =
+            new List<TransitioningTriggerBehaviour<TState, TTrigger>>();
+
+        private IDictionary<TTrigger, ICollection<TransitioningTriggerBehaviour<TState, TTrigger>>> _triggerBehaviours;
+        /// <summary>
+        /// 触发器行为
+        /// </summary>
+        public IDictionary<TTrigger, ICollection<TransitioningTriggerBehaviour<TState, TTrigger>>> TriggerBehaviours
+        {
+            get
+            {
+                if (_triggerBehaviours == null)
+                {
+                    _triggerBehaviours = new Dictionary<TTrigger, ICollection<TransitioningTriggerBehaviour<TState, TTrigger>>>();
+                    foreach (var group in Behaviours.GroupBy(s => s.Trigger))
+                    {
+                        _triggerBehaviours.Add(group.Key, group.ToList());
+                    }
+                }
+
+                return _triggerBehaviours;
+            }
+        }
+
+
+        /// <summary>
+        /// Accept the specified trigger and transition to the destination state.
+        /// </summary>
+        /// <param name="trigger">The accepted trigger.</param>
+        /// <param name="destinationState">The state that the trigger will cause a transition to.</param>
+        /// <returns>The receiver.</returns>
+        public StateRepresentation<TState, TTrigger> Permit(TTrigger trigger, TState destinationState)
+        {
+            EnforceNotIdentityTransition(destinationState);
+            AddTriggerBehaviour(new TransitioningTriggerBehaviour<TState, TTrigger>(trigger, destinationState));
+            return this;
+        }
+
+        private void AddTriggerBehaviour(TransitioningTriggerBehaviour<TState, TTrigger> triggerBehaviour)
+        {
+            if (!TriggerBehaviours.TryGetValue(triggerBehaviour.Trigger, out ICollection<TransitioningTriggerBehaviour<TState, TTrigger>> allowed))
+            {
+                allowed = new List<TransitioningTriggerBehaviour<TState, TTrigger>>();
+                TriggerBehaviours.Add(triggerBehaviour.Trigger, allowed);
+            }
+
+            allowed.Add(triggerBehaviour);
+            Behaviours.Add(triggerBehaviour);
+        }
+
+
+        public bool TryFindBehaviour(TTrigger trigger, out ICollection<TransitioningTriggerBehaviour<TState, TTrigger>> triggerBehaviours)
+        {
+            // Get list of candidate trigger handlers
+            if (!TriggerBehaviours.TryGetValue(trigger, out ICollection<TransitioningTriggerBehaviour<TState, TTrigger>> possible))
+            {
+                triggerBehaviours = null;
+                return false;
+            }
+
+            triggerBehaviours = possible;
+            return true;
+        }
+
+        private void EnforceNotIdentityTransition(TState destination)
+        {
+            if (destination.Equals(State))
+            {
+                throw new ArgumentException($"目标状态不能等于原状态 {State}");
+            }
+        }
+    }
+}
